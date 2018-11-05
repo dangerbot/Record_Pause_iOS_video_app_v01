753427e13f50b508621d3c835c96cbe9d00cfeb9
Steve <dangerboy@macbook-pro-3.local>
2018-11-05 08:07:32 -0800

    Initial Commit

---
+++ Record_Pause_iOS_video_app_v01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Record_Pause_iOS_video_app_v01
+//
+//  Created by Steve on 11/5/18.
+//  Copyright © 2018 SteveAndTheDogs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
